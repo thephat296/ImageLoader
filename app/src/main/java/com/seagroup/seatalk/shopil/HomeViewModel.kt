d17ea3703215712e18155a4291432f4e87a9145f
Phat Voong <thephat.voong@shopee.com>
2021-06-01 12:01:05 +0700

    demo fetch urls

--- app/src/main/java/com/seagroup/seatalk/shopil/HomeViewModel.kt
+++ app/src/main/java/com/seagroup/seatalk/shopil/HomeViewModel.kt
@@ -4,9 +4,30 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
 
 class HomeViewModel : ViewModel() {
+    private val httpClient by lazy(::OkHttpClient)
+
     fun loadImages(): LiveData<List<String>> = liveData(viewModelScope.coroutineContext) {
-        emit(listOf("https://picsum.photos/id/119/3264/2176"))
+        withContext(Dispatchers.IO) {
+            val rawData = httpClient
+                .newCall(request = Request.Builder().url(URL_IMAGES).build())
+                .execute()
+                .use { response ->
+                    response.body?.string()
+                } ?: return@withContext
+            val urls = JSONArray(rawData).toList(KEY_DOWNLOAD_URL)
+            emit(urls)
+        }
+    }
+
+    companion object {
+        private const val URL_IMAGES = "https://picsum.photos/v2/list?page=2&limit=100"
+        private const val KEY_DOWNLOAD_URL = "download_url"
     }
 }
