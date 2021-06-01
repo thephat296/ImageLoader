a4d11a5c0ba5dbcf0bf280e34d3041dd33f07647
Phat Voong <thephat.voong@shopee.com>
2021-06-01 18:53:38 +0700

    refactor mem-cache

--- shopil/src/main/java/com/seagroup/seatalk/shopil/memory/DefaultMemoryCache.kt
+++ shopil/src/main/java/com/seagroup/seatalk/shopil/memory/DefaultMemoryCache.kt
@@ -5,17 +5,14 @@ import androidx.collection.LruCache
 import com.seagroup.seatalk.shopil.key.CacheKey
 
 class DefaultMemoryCache(maxSize: Int) : MemoryCache {
-    private val cache = LruCache<CacheKey, Bitmap>(maxSize)
+    private val cache = object : LruCache<CacheKey, Bitmap>(maxSize) {
+        override fun sizeOf(key: CacheKey, value: Bitmap) = value.allocationByteCount
+    }
 
     override fun get(key: CacheKey): Bitmap? = cache.get(key)
 
     override fun set(key: CacheKey, bitmap: Bitmap) {
-        val size = bitmap.allocationByteCount
-        if (size > cache.maxSize()) {
-            cache.remove(key) // evict an existing element with the same key if it exists.
-        } else {
-            cache.put(key, bitmap)
-        }
+        cache.put(key, bitmap)
     }
 
     override fun remove(key: CacheKey): Boolean = cache.remove(key) != null
